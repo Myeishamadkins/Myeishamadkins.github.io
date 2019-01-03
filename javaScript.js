b8fba265224eee5290b4ab49bdc67e169ce4caec
Myeishamadkins <mmadkins@basecamecodingacademy.org>
2019-01-03 08:38:06 -0600

    Fixed issues on github:
    changes to Index.html:
    added a tags around my images so that the user could click on the
     image and the link would go to my github page.
    
    chages to javaScript.js:
    1. added gitHubUrl links to my PAGE_DATA for pythonProjectContent &
    htmlCssJavascriptProjectContent.
    
    2. added gitHubUrl and gitHubUrlA to pythonProjectContent function &
    htmlCssJavascriptProjectContent function.

--- javaScript.js
+++ javaScript.js
@@ -3,38 +3,47 @@ const PAGE_DATA = {
     pythonProjectContent: [
         {
             imageUrl: "./images/tetris.png",
+            gitHubUrl: "https://github.com/Myeishamadkins/Tetris",
             title: "Tetris",
             language: "Python",
             description: "Made with tkinter"
         },
         {
             imageUrl: "./images/rental_store.png",
+            gitHubUrl:
+                "https://github.com/Myeishamadkins/Rental_Agency_MyeishaMadkins",
             title: "Rental Agency",
             language: "Python",
             description: "Python Final Project"
         },
         {
             imageUrl: "./images/gladiator.png",
+            gitHubUrl: "https://github.com/Myeishamadkins/Gladiator-game",
             title: "Gladiator Game",
             language: "Python",
             description: "Working on dictionaries in python"
         },
         {
             imageUrl: "./images/blackjack.png",
+            gitHubUrl: "https://github.com/Myeishamadkins/Blackjack",
             title: "Blackjack",
             language: "Python",
-            description: "Made after a mouth of programming"
+            description: "Made after a month of programming"
         }
     ],
     htmlCssJavascriptProjectContent: [
         {
             imageUrl: "./images/homepage.png",
+            gitHubUrl:
+                "https://github.com/Myeishamadkins/Myeisha-Madkins-Web-Rental-Agency",
             title: "Front End Final Project",
             language: "HTML, CSS, JavaScript",
             description: "Made using HTML, CSS, JavaScript, & Boostrap"
         },
         {
             imageUrl: "./images/bookstore.png",
+            gitHubUrl:
+                "https://github.com/Myeishamadkins/Book_store_replication",
             title: "Bookstore",
             language: "Django",
             description:
@@ -42,12 +51,16 @@ const PAGE_DATA = {
         },
         {
             imageUrl: "./images/slackfeatures.png",
+            gitHubUrl:
+                "https://github.com/Myeishamadkins/Component-Practice-SlackFeatures",
             title: "Slack Features",
             language: "HTML & CSS",
             description: "Using HTML & CSS to resemble a picture"
         },
         {
             imageUrl: "./images/GithubLogin.png",
+            gitHubUrl:
+                "https://github.com/Myeishamadkins/Component-Practice-GithubLogin",
             title: "Github Login",
             language: "HTML & CSS",
             description:
@@ -55,24 +68,32 @@ const PAGE_DATA = {
         },
         {
             imageUrl: "./images/DevtoArticleCard.png",
+            gitHubUrl:
+                "https://github.com/Myeishamadkins/Component-Practice-DevtoArticleCard",
             title: "Devto Article Card",
             language: "HTML & CSS",
             description: "Made using HTML & CSS to resemble a picture"
         },
         {
             imageUrl: "./images/pypitrending.png",
+            gitHubUrl:
+                "https://github.com/Myeishamadkins/Component-Practice-PypiTrendingNewProjects",
             title: "Pypi Trending",
             language: "HTML & CSS",
             description: "Made using HTML & CSS to resemble a picture"
         },
         {
             imageUrl: "./images/key links.png",
+            gitHubUrl:
+                "https://github.com/Myeishamadkins/Component-Practice-DevtoKeyLinks",
             title: "Devto Key Links",
             language: "HTML & CSS",
             description: "Made using HTML & CSS to resemble a picture"
         },
         {
             imageUrl: "./images/explainlikeimfive.png",
+            gitHubUrl:
+                "https://github.com/Myeishamadkins/Component-Practice-DevtoELI5",
             title: "Explain like I'm five",
             language: "HTML & CSS",
             description: "Made using HTML & CSS to resemble a picture"
@@ -87,10 +108,12 @@ function pythonProjectContent() {
     for (var i = 0; i < PAGE_DATA.pythonProjectContent.length; i += 2) {
         content += template({
             imageUrl: PAGE_DATA.pythonProjectContent[i].imageUrl,
+            gitHubUrl: PAGE_DATA.pythonProjectContent[i].gitHubUrl,
             title: PAGE_DATA.pythonProjectContent[i].title,
             language: PAGE_DATA.pythonProjectContent[i].language,
             description: PAGE_DATA.pythonProjectContent[i].description,
             imageUrlA: PAGE_DATA.pythonProjectContent[i + 1].imageUrl,
+            gitHubUrlA: PAGE_DATA.pythonProjectContent[i + 1].gitHubUrl,
             titleA: PAGE_DATA.pythonProjectContent[i + 1].title,
             languageA: PAGE_DATA.pythonProjectContent[i + 1].language,
             descriptionA: PAGE_DATA.pythonProjectContent[i + 1].description
@@ -113,12 +136,15 @@ function htmlCssJavascriptProjectContent() {
     ) {
         content += template({
             imageUrl: PAGE_DATA.htmlCssJavascriptProjectContent[i].imageUrl,
+            gitHubUrl: PAGE_DATA.htmlCssJavascriptProjectContent[i].gitHubUrl,
             title: PAGE_DATA.htmlCssJavascriptProjectContent[i].title,
             language: PAGE_DATA.htmlCssJavascriptProjectContent[i].language,
             description:
                 PAGE_DATA.htmlCssJavascriptProjectContent[i].description,
             imageUrlA:
                 PAGE_DATA.htmlCssJavascriptProjectContent[i + 1].imageUrl,
+            gitHubUrlA:
+                PAGE_DATA.htmlCssJavascriptProjectContent[i + 1].gitHubUrl,
             titleA: PAGE_DATA.htmlCssJavascriptProjectContent[i + 1].title,
             languageA:
                 PAGE_DATA.htmlCssJavascriptProjectContent[i + 1].language,
